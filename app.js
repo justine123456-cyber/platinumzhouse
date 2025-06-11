6f8536b5f9b52dab56c155a8cde74009fc8223dc
justine123456-cyber <justineokeny236@gmail.com>
2025-06-12 01:42:42 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let iconCart = document.querySelector('.iconCart');
+let cart = document.querySelector('.cart');
+let container = document.querySelector('.container')
+let close = document.querySelector('.close');
+
+iconCart.addEventListener('click', ()=>{
+	if (cart.style.right=='-100%') {
+		cart.style.right = 
+	}
+})
